55a7958995c0702f78fbbb935cfea96b8bb5ace1
Rafael Machado <68705814+dev-rafaelmachado@users.noreply.github.com>
2024-04-23 21:05:33 -0300

    fix: double time

--- charts.py
+++ charts.py
@@ -7,9 +7,9 @@ sizes = [i * T for i in range(N)]
 
 def plot_algorithm_progression(results: List[List[float]], title: str, filename: str) -> None:
     for idx, algorithm in enumerate(algorithms):
-        chart = [x[idx] for x in results]
-        plt.plot(sizes, chart, label=algorithm)
-        plt.plot([2 * size for size in sizes], chart, linestyle='--', label='Double the Data')
+        times = [x[idx] for x in results]
+        plt.plot(sizes, times, label=algorithm)
+        plt.plot([2 * size for size in sizes], [2 * time for time in times], linestyle='--', label='Double the Data')
 
         plt.xlabel('Data Size')
         plt.ylabel('Time Spent')
